0cd61916cec43512f6a3ffee04813d0c6031f401
Xiao Haiping <xiaohp1993@gmail.com>
2017-07-09 11:02:11 +0800

    添加数组方法

--- lodash/lodash.js
+++ lodash/lodash.js
@@ -1,22 +1,22 @@
 // 切分数组
-var chunk = function(array, size=1) {
-    var length = array.length
+const chunk = function(array, size=1) {
+    let length = array.length
     if (length === 0 || size < 1 ) {
         return []
     } else {
-        var res = []
-        for (var i = 0; i < array.length; i += size) {
-            var l = array.slice(i, i + size)
+        let res = []
+        for (let i = 0; i < array.length; i += size) {
+            let l = array.slice(i, i + size)
             res.push(l)
         }
         return res
     }
 }
 // 删除否定值 false, null, 0, "", undefined, NaN
-var compact = function(array) {
+const compact = function(array) {
     var res = []
-    for (var i = 0; i < array.length; i++) {
-        var a = array[i]
+    for (let i = 0; i < array.length; i++) {
+        let a = array[i]
         if (a) {
             res.push(a)
         }
@@ -24,10 +24,10 @@ var compact = function(array) {
     return res
 }
 // 返回一个数组，由在 a 数组中不在 b 数组中的元素组成
-var difference = function(a, b) {
-    var res = []
-    for (var i = 0; i < a.length; i++) {
-        var l = a[i]
+const difference = function(a, b) {
+    let res = []
+    for (let i = 0; i < a.length; i++) {
+        let l = a[i]
         if (!b.includes(l)) {
             res.push(l)
         }
@@ -35,18 +35,18 @@ var difference = function(a, b) {
     return res
 }
 // 返回从指定位置截取的数组
-var drop = function(array, n=1) {
+const drop = function(array, n=1) {
     return array.slice(n)
 }
 // 拍平数组
 // 方法一
 // var flatten = function(array) {
-//     var res = []
-//     for (var i = 0; i < array.length; i++) {
-//         var a = array[i]
+//     let res = []
+//     for (let i = 0; i < array.length; i++) {
+//         let a = array[i]
 //         if (Array.isArray(a)) {
-//             for (var j = 0; j < a.length; j++) {
-//                 var b = a[j]
+//             for (let j = 0; j < a.length; j++) {
+//                 let b = a[j]
 //                 res.push(b)
 //             }
 //         } else {
@@ -57,7 +57,7 @@ var drop = function(array, n=1) {
 // }
 // 方法二
 // var flatten = function(array) {
-//     var res = array.reduce(function(a, b) {
+//     let res = array.reduce(function(a, b) {
 //         return a.concat(b)
 //     }, [])
 //     return res
@@ -75,3 +75,62 @@ const flatten = list => list.reduce(
 const flatten_deep = list => list.reduce(
     (a, b) => a.concat(Array.isArray(b) ? flatten_deep(b) : b), []
 )
+// 将数组拼成键值对
+const from_pairs = function(pairs) {
+    let res = {}
+    for (let i = 0; i < pairs.length; i++) {
+        let pair = pairs[i]
+        res[pair[0]] = pair[1]
+    }
+    return res
+}
+// 返回数组第一个元素
+const head = function(array) {
+    return array[0]
+}
+// 返回元素在数组中第一次出现的位置，可以设置从指定位置开始查找
+const indexOf = function(array, value, from_index=0) {
+    for (let i = from_index; i < array.length; i++) {
+        let a = array[i]
+        if (a === value) {
+            return i
+        }
+    }
+    return -1
+}
+// 返回不包含最后一个元素的数组
+const initial = function(array) {
+    let l = array.length - 1
+    return array.slice(0, l)
+}
+// 数组求交集
+const intersection = function(a, b) {
+    const _unique = function(array) {
+        return Array.from(new Set(array))
+    }
+    let unique_a = _unique(a)
+    let res = []
+    for (let i = 0; i < unique_a.length; i++) {
+        let ai = unique_a[i]
+        if (b.includes(ai)) {
+            res.push(ai)
+        }
+    }
+    return res
+}
+// 将数组拼接成字符串
+const join = function(array, separator=',') {
+    return array.join(separator)
+}
+// 获取数组最后一个元素
+const last = function(array) {
+    return array[array.length - 1]
+}
+// 获取数组的第 n 个元素，如果 n 是负数，则返回的是从末尾开始查找的元素
+const nth = function(array, n=0) {
+    if (n >= 0) {
+        return array[n]
+    } else {
+        return array[n + array.length]
+    }
+}
--- lodash/test.js
+++ lodash/test.js
@@ -8,16 +8,16 @@ var ensure = function(a, b, message) {
 var test_chunk = function() {
     var numbers = [1, 2, 3, 4]
     var list = [1, 2, 3, 4, 5]
-    ensure(chunk(numbers, 2), [[1, 2], [3, 4]], 'chunk 测试 1 出错')
-    ensure(chunk(list, 3), [[1, 2, 3], [4, 5]], 'chunk 测试 2 出错')
-    ensure(chunk(list, 6), [[1, 2, 3, 4, 5]], 'chunk 测试 3 出错')
+    ensure(chunk(numbers, 2), [[1, 2], [3, 4]], 'chunk test 1 error')
+    ensure(chunk(list, 3), [[1, 2, 3], [4, 5]], 'chunk test 2 error')
+    ensure(chunk(list, 6), [[1, 2, 3, 4, 5]], 'chunk test 3 error')
 }
 
 test_chunk()
 
 var test_compact = function() {
     var numbers = [0, 1, false, 2, '', 3]
-    ensure(compact(numbers), [1, 2, 3], 'compact 测试 1 出错')
+    ensure(compact(numbers), [1, 2, 3], 'compact test 1 error')
 }
 
 test_compact()
@@ -25,27 +25,81 @@ test_compact()
 var test_difference = function() {
     var a = [1, 2]
     var b = [2, 3]
-    ensure(difference(a, b), [1], 'difference 测试 1 出错')
+    ensure(difference(a, b), [1], 'difference test 1 error')
 }
 
 test_difference()
 
 var test_drop = function() {
-    ensure(drop([1, 2, 3]), [2, 3], 'drop 测试 1 出错')
-    ensure(drop([1, 2, 3], 2), [3], 'drop 测试 2 出错')
-    ensure(drop([1, 2, 3], 5), [], 'drop 测试 3 出错')
+    ensure(drop([1, 2, 3]), [2, 3], 'drop test 1 error')
+    ensure(drop([1, 2, 3], 2), [3], 'drop test 2 error')
+    ensure(drop([1, 2, 3], 5), [], 'drop test 3 error')
 }
 
 test_drop()
 
 var test_flatten = function() {
-    ensure(flatten([1, [2, [3, [4]], 5]]), [1, 2, [3, [4]], 5], 'flatten 测试 1 出错')
+    ensure(flatten([1, [2, [3, [4]], 5]]), [1, 2, [3, [4]], 5], 'flatten test 1 error')
 }
 
 test_flatten()
 
 var test_flatten_deep = function() {
-    ensure(flatten_deep([1, [2, [3, [4]], 5]]), [1, 2, 3, 4, 5], 'flatten_deep 测试 1 出错')
+    ensure(flatten_deep([1, [2, [3, [4]], 5]]), [1, 2, 3, 4, 5], 'flatten_deep test 1 error')
 }
 
 test_flatten_deep()
+
+var test_from_paris = function() {
+    let pairs = [['a', 1], ['b', 2]]
+    ensure(from_pairs(pairs), { 'a': 1, 'b': 2 }, 'from_pairs test1 error')
+}
+
+test_from_paris()
+
+var test_head = function() {
+    ensure(head([1, 2, 3]), 1, 'head test 1 error')
+    ensure(head([]), undefined, 'head test 2 error')
+}
+
+test_head()
+
+var test_indexOf = function() {
+    ensure(indexOf([1, 2, 1, 2], 2), 1, 'indexOf test 1 error')
+    ensure(indexOf([1, 2, 1, 2], 2, 2), 3, 'indexOf test 2 error')
+}
+
+test_indexOf()
+
+var test_initial = function() {
+    ensure(initial([1, 2, 3]), [1, 2], 'initial test 1 error')
+}
+
+test_initial()
+
+var test_intersection = function() {
+    ensure(intersection([2, 1, 2, 4], [2, 3, 4]), [2, 4], 'intersection test 1 error')
+}
+
+test_intersection()
+
+var test_join = function() {
+    ensure(join(['a', 'b', 'c'], '~'), 'a~b~c', 'join test 1 error')
+    ensure(join(['a', 'b', 'c'], ' '), 'a b c', 'join test 2 error')
+    ensure(join(['a', 'b', 'c']), 'a,b,c', 'join test 3 error')
+}
+
+test_join()
+
+var test_last = function() {
+    ensure(last([1, 2, 3]), 3, 'last test 1 error')
+}
+
+test_last()
+
+var test_nth = function() {
+    let array = ['a', 'b', 'c', 'd']
+    ensure(nth(array, 1), 'b', 'nth test 1 error')
+    ensure(nth(array, -2), 'c', 'nth test 2 error')
+}
+test_nth()
