e626b86481a837a69f7443f18a2d3b6b9c4e769c
Xiao Haiping <xiaohp1993@gmail.com>
2017-07-11 11:10:09 +0800

    add gulp demo

---
+++ gulp-demo/gulpfile.js
@@ -0,0 +1,18 @@
+// 获取 gulp
+var gulp = require('gulp')
+var rename = require('gulp-rename')
+var uglify = require('gulp-uglify')
+// 压缩 js 文件
+// 在命令行使用 gulp script 启动此任务
+gulp.task('script', function() {
+    // 1. 找到文件
+    gulp.src('js/*.js')
+        // 压缩文件
+        .pipe(uglify())
+        // 重命名压缩后的文件
+        // .pipe(rename({ 
+        //     extname: '.min.js' 
+        // }))
+        // 另存压缩后的文件
+        .pipe(gulp.dest('dist/js'))
+})
