702a0564cac80a70f8a21cbccf1ea0dc3c2d726c
Xiao Haiping <xiaohp1993@gmail.com>
2017-07-11 13:59:18 +0800

    enable rename

--- gulp-demo/gulpfile.js
+++ gulp-demo/gulpfile.js
@@ -5,14 +5,14 @@ var uglify = require('gulp-uglify')
 // 压缩 js 文件
 // 在命令行使用 gulp script 启动此任务
 gulp.task('script', function() {
-    // 1. 找到文件
+    // 找到文件
     gulp.src('js/*.js')
         // 压缩文件
         .pipe(uglify())
         // 重命名压缩后的文件
-        // .pipe(rename({ 
-        //     extname: '.min.js' 
-        // }))
+        .pipe(rename({ 
+            extname: '.min.js' 
+        }))
         // 另存压缩后的文件
         .pipe(gulp.dest('dist/js'))
 })
