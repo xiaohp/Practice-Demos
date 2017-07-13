0763db9bbe1c64f2bf480694fcf15146c3dab7d1
Xiao Haiping <xiaohp1993@gmail.com>
2017-07-13 17:40:24 +0800

    add sorting algorithm

---
+++ sorting-algorithm/sort.js
@@ -0,0 +1,79 @@
+// 工具函数，交换数组的值
+const swap = function(array, p1, p2) {
+    [array[p1], array[p2]] = [array[p2], array[p1]]
+}
+
+// 冒泡排序
+const bubble_sort = function(array) {
+    let length = array.length
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                swap(array, j, j + 1)
+            }
+        }
+    }
+    return array
+}
+
+// 选择排序
+const selection_sort = function(array) {
+    let length = array.length
+    for (let i = 0; i < length; i++) {
+        let min = i
+        for (let j = i + 1; j < length; j++) {
+            if (array[j] < array[min]) {
+                min < j
+            }
+        }
+        if (i !== min) {
+            swap(array, i, min)
+        }
+    }
+    return array
+}
+
+// 插入排序
+const insertion_sort = function(array) {
+    let length = array.length
+    let j
+    for (let i = 0; i < length; i++) {
+        let value = array[i]
+        for ( j = i - 1; j > -1 && array[j] > value; j--) {
+            array[j + 1] = array[j]
+        }
+        array[j + 1] = value
+    }
+    return array
+}
+
+// 合并排序
+// 合并排序工具函数，合并数组
+const merge = function(left, right) {
+    let res = []
+    let index_l = 0
+    let index_r = 0
+    while (index_l < left.length && index_r < right.length) {
+        if (left[index_l] < right[index_r]) {
+            res.push(left[index_l])
+            index_l++
+        } else {
+            res.push(right[index_r])
+            index_r++
+        }
+    }
+    let remain_left = left.slice(index_l)
+    let remain_right = right.slice(index_r)
+    return res.concat(remain_left).concat(remain_right)
+}
+
+const merge_sort = function(array) {
+    if (array.length < 2) {
+        return array
+    }
+    
+    let middle = Math.floor(array.length / 2)
+    let left = array.slice(0, middle)
+    let right = array.slice(middle)
+    return merge(merge_sort(left), merge_sort(right))
+}
---
+++ sorting-algorithm/test.js
@@ -0,0 +1,31 @@
+const log = console.log.bind(console, '*** debug ***')
+
+const ensure = function(a, b, message) {
+    let condition = JSON.stringify(a) === JSON.stringify(b)
+    if (!condition) {
+        log(message, `${a} 不等于 ${b}`)
+    }
+}
+
+let a = [4, 15, 6, 49, 2]
+let res = [2, 4, 6, 15, 49]
+
+const test_bubble_sort = function() {
+    ensure(bubble_sort(a), res, 'bubble_sort test 1 error')
+}
+test_bubble_sort()
+
+const test_selection_sort = function() {
+    ensure(selection_sort(a), res, 'selection_sort test 1 error')
+}
+test_selection_sort()
+
+const test_insertion_sort = function() {
+    ensure(insertion_sort(a), res, 'insertion_sort test 1 error')
+}
+test_insertion_sort()
+
+const test_merge_sort = function() {
+    ensure(merge_sort(a), res, 'merge_sort test 1 error')
+}
+test_merge_sort()
