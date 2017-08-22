bc6ffed190a3df218e5c96976541e1695138d897
Xiao Haiping <xiaohp1993@gmail.com>
2017-08-22 09:11:45 +0800

    rename task

--- gulp-demo/gulpfile.js
+++ gulp-demo/gulpfile.js
@@ -4,17 +4,17 @@ var rename = require('gulp-rename')
 var uglify = require('gulp-uglify')
 // 压缩 js 文件
 // 在命令行使用 gulp script 启动此任务
-gulp.task('minify', function() {
+gulp.task('uglify', function() {
     // 找到文件
     gulp.src('js/*.js')
         // 压缩文件
         .pipe(uglify())
         // 重命名压缩后的文件
-        .pipe(rename({ 
-            extname: '.min.js' 
+        .pipe(rename({
+            extname: '.min.js'
         }))
         // 另存压缩后的文件
         .pipe(gulp.dest('dist/js'))
 })
 
-gulp.task('default', ['minify'])
+gulp.task('default', ['uglify'])
